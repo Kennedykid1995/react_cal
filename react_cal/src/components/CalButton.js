0a0e1882760e1a08f6057ca132a15a3069bd40f3
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-01-07 22:32:33 -0600

    basic structure has been made, functionality will be built

--- react_cal/src/components/CalButton.js
+++ react_cal/src/components/CalButton.js
@@ -1,31 +1,40 @@
 import React from 'react'
+import CalInput from './CalInput'
 
 const Buttons = () => {
     return (
         <div>
+            <section>
+                <CalInput />
+            </section>
             <section className="cal-row">
                 <div className="cal-btn">AC</div>
                 <div className="cal-btn">+/-</div>
                 <div className="cal-btn">%</div>
+                <div className="cal-btn">/</div>
             </section>
             <section className="cal-row">
                 <div className="cal-btn">1</div>
                 <div className="cal-btn">2</div>
                 <div className="cal-btn">3</div>
+                <div className="cal-btn">X</div>
             </section>
             <section className="cal-row">
                 <div className="cal-btn">4</div>
                 <div className="cal-btn">5</div>
                 <div className="cal-btn">6</div>
+                <div className="cal-btn">-</div>
             </section>
             <section className="cal-row">
                 <div className="cal-btn">7</div>
                 <div className="cal-btn">8</div>
                 <div className="cal-btn">9</div>
+                <div className="cal-btn">+</div>
             </section>
             <section className="cal-row">
                 <div className="cal-btn-lng">0</div>
                 <div className="cal-btn">.</div>
+                <div className="cal-btn">=</div>
             </section>
         </div>
     )
---
+++ react_cal/src/components/CalInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CalInput = () => {
+    return (
+        <div className="cal-input">
+        0
+        </div>
+    )
+}
+
+export default CalInput; 
